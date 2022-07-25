94edc18913723dbefd72d4b296f7a657a3893a9f
HSL <274383601@qq.com>
2022-07-25 09:28:03 +0800

    添加主界面测试控件使用

--- app/src/main/java/cat/customize/activity/CtMainActivity.java
+++ app/src/main/java/cat/customize/activity/CtMainActivity.java
@@ -1,13 +1,15 @@
-package cat.customize;
+package cat.customize.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import cat.customize.R;
+
+public class CtMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.ct_activity_main);
     }
 }
